2805586e4bba760fa9b13874356581c3a5a00523
Luke McKenzie <luke@theclockspot.com>
2018-10-14 15:36:35 -0500

    Retire mainSelFn, mainAdjFn, altSelFn. Their functions are baked-in now.

--- sixtube_lm/configs/v8b-6tube-relayswitch-pwm-top.h
+++ sixtube_lm/configs/v8b-6tube-relayswitch-pwm-top.h
@@ -25,21 +25,14 @@ const byte fnsEnabled[] = {fnIsTime, fnIsDate, fnIsAlarm, fnIsTimer, fnIsDayCoun
 // A6-A7 are analog-only pins that aren't quite as responsive and require a physical pullup resistor (1K to +5V), and can't be used with rotary encoders because they don't support pin change interrupts.
 
 // What input is associated with each control?
-const byte mainSel = A7; //main select button - must be equipped
-const byte mainAdjUp = A0; //main up/down buttons or rotary encoder - must be equipped
+const byte mainSel = A7;
+const byte mainAdjUp = A0;
 const byte mainAdjDn = A1;
-const byte altSel = A6; //alt select button - if unequipped, set to 0
+const byte altSel = A6; //if not equipped, set to 0
 
 // What type of adj controls are equipped?
 // 1 = momentary buttons. 2 = quadrature rotary encoder.
 const byte mainAdjType = 1;
-
-// In normal running mode, what do the controls do?
-// -1 = nothing/switch, -2 = cycle through functions, fn in fnsEnabled[] = go to that function
-// If using soft alarm/power switch per below, the control(s) set to -1 will do the switching.
-const char mainSelFn = -2;
-const char mainAdjFn = -1;
-const byte altSelFn = fnIsAlarm; //go straight to alarm
 
 //What are the signal pin(s) connected to?
 const char piezoPin = 10;
